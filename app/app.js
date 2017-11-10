1f8a5e13af1f47a42eca459a96f5dd2871d3adce
mariaimr <mimr0694@gmail.com>
2017-11-09 20:38:26 -0500

    se añade la ruta /

--- app/app.js
+++ app/app.js
@@ -4,4 +4,8 @@ let app = express();
 require('../config/config')(app);
 require('../endpoints/routes')(app);
 
+app.get('/', (req, res) => {
+	return res.status(200).json({'hola': 'heroku'});
+})
+
 module.exports = app;
