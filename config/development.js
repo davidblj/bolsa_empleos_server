10da2820d4dc778e28439895e6812ae53fff19e1
mariaimr <mimr0694@gmail.com>
2017-11-09 20:06:08 -0500

    fix bug

--- config/development.js
+++ config/development.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret':  'omega',
     'port': 3000,
-    'database': 'mongodb://localhost:27017/bolsa-de-empleos'
+    'database': 'mongodb://bolsaudea:bolsaudea@ds249325.mlab.com:49325/bolsa_empleos'
 };
