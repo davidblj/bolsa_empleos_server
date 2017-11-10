6f0aa08dfeba4021ee5c21ae4631c4811f2873d3
mariaimr <mimr0694@gmail.com>
2017-11-09 20:31:54 -0500

    fix port

--- config/development.js
+++ config/development.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret':  'omega',
-    'port': 3000,
+    'port': process.env.PORT || 3000,
     'database': 'mongodb://bolsaudea:bolsaudea@ds249325.mlab.com:49325/bolsa_empleos'
 };
