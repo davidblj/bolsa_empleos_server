67ba60fc37527c8329179988bf907a1bf62b8365
davidblj <david.blj95@gmail.com>
2018-01-09 10:38:52 -0500

    mongo script

---
+++ mongoScript.js
@@ -0,0 +1,5 @@
+conn = new Mongo();
+db = conn.getDB("bolsa-de-empleos-test");
+
+printjson(db.jobs.createIndex({jobName: "text", ownerCompany: "text"}));
+printjson(db.jobs.createIndex({salary: -1, _id: -1}));
