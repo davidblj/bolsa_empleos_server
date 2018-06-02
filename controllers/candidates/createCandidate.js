9c446f4fd2e51990d7e471549f55cdf8506f7eee
davidblj <david.blj95@gmail.com>
2018-06-02 12:18:58 -0500

    agregar utilidad file
    Se generalizan las utilidades para el manejo de archivos

--- controllers/candidates/createCandidate.js
+++ controllers/candidates/createCandidate.js
@@ -6,6 +6,7 @@ const {
     findEmptyArguments,
     field } = require(process.cwd() + '/utils/validations/controller-validations');
 const log = require(process.cwd() + '/utils/debug');
+const { resumeeProcessing } = require(process.cwd() + '/utils/file');
 const fileSystem = require('fs');
 
 // services
@@ -55,49 +56,4 @@ module.exports = async (data, file) => {
 function validate(data, file) {
     findEmptyArguments([field(data, 'data'), field(file, 'file')]);
     findEmptyFields(data, ['username', 'email']);
-}
-
-/**
- * Resumee file processing. This processing just moves the resumee from the staging folder
- * area to a fixed file path: 'resumes/:id/resume'
- * @private
- * @param {File} file - the resumee file to move
- * @param {string} id - the proprietary user id
- */
-function resumeeProcessing(file, id) {
-
-    let filename = file.filename;
-    let extension  = file.mimetype.split('/')[1];
-
-    let rootPath = process.cwd();
-    let oldPath = `${rootPath}/resumes/staging/${filename}`;
-    let destination = `${rootPath}/resumes/${id}`;
-    let newPath = `${destination}/cv.${extension}`;
-
-    mkdirp(destination, moveFile(oldPath, newPath));
-}
-
-/**
- * Callback utility (through "Javascript currying"). This callback contains
- * the actual logic to move files from an old path, to a new path.
- * @private
- * @param {string} oldPath - the original path where the current file is standing
- * @param {string} newPath - the new path where the current file will be moved to
- * @return {function(error, created)} - a callback function to be executed by "mkdirp"
- */
-function moveFile(oldPath, newPath) {
-
-    return (error, created) => {
-
-        if (error) {
-            throw error;
-        }
-
-        fileSystem.rename(oldPath, newPath, (error) => {
-            if (error) throw error;
-            log.common(
-                `file moved from: ${oldPath} \n` +
-                `to: ${newPath}`)
-        });
-    }
 }
